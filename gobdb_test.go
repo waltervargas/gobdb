f9a913eab3773c7d71d502babfb42fbce85c6845
Walter Vargas <walter.vargas@n26.com>
2023-03-13 19:32:21 +0100

    Add tests

--- gobdb_test.go
+++ gobdb_test.go
@@ -62,6 +62,40 @@ func TestAddStruct(t *testing.T) {
 		Prio Prio
 	}
 
+	todo := Todo{Name: "buy milk", Prio: Prio{"High"}}
+	want := gobdb.Data{
+		"todo": todo,
+	}
+
+	//db.AddSchema(todos)
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestAddStructList(t *testing.T) {
+	t.Parallel()
+
+	tmp := t.TempDir() + "/addStruct.gobdb"
+	db, err := gobdb.Open(tmp)
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+
+	type Prio struct {
+		Name string
+	}
+
+	type Todo struct {
+		Name string
+		Prio Prio
+	}
+
 	todos := []Todo{
 		{Name: "buy milk", Prio: Prio{"High"}},
 		{Name: "tax calculation", Prio: Prio{"High"}},
