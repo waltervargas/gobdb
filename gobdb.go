1917615c8926e2b42022e9ca4ddb882ceea902de
Walter Vargas <w@walter.bio>
2023-03-16 14:54:36 +0100

    Add basic ops: Open, List, Add (#1)

---
+++ gobdb.go
@@ -0,0 +1,78 @@
+// Package gobdb provides a simple key-value database for Go binary objects
+// encoded with encoding/gob.
+package gobdb
+
+import (
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+// gobdb represents a simple key-value database for Go binary objects encoded
+// with encoding/gob.
+type gobdb struct {
+	Data Data
+	path string
+}
+
+// Data is a type alias for a map with keys and values of any type.
+type Data map[any]any
+
+// Open opens a file at the specified path and decodes its contents using the
+// gob decoder.
+//
+// If the decoding fails, it returns an error.
+// If the decoding is successful, it returns a gobdb object with the decoded
+// data.
+func Open(path string) (gobdb, error)  {
+	file, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0755)
+	if err != nil {
+		return gobdb{}, err
+	}
+	defer file.Close()
+
+	decoder := gob.NewDecoder(file)
+	data := make(Data)
+	err = decoder.Decode(&data)
+	if err != nil {
+		if !errors.Is(err, io.EOF) {
+			return gobdb{}, err
+		}
+	}
+	return gobdb{Data: data, path: path}, nil
+}
+
+// List returns the entire database as a map.
+func (db gobdb) List() Data {
+	return db.Data
+}
+
+// Add append a new key-value pair of data to the db and persist data to disk
+// using the given `path` to Open()
+func (db *gobdb) Add(d Data) error {
+	for k, v := range d {
+		db.Data[k] = v
+
+		// if the caller uses a custom type such as struct as value, then we
+		// need to register this value to gob so it an encode it, otherwise:
+		//
+		//    unable to encode collection: gob: type not registered for interface: []gobdb_test.Todo
+		//
+		gob.Register(v)
+	}
+	file, err := os.Create(db.path)
+	if err != nil {
+		return fmt.Errorf("unable to open file: %w", err)
+	}
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(&db.Data)
+	if err != nil {
+		return fmt.Errorf("unable to encode collection: %w", err)
+	}
+
+	return nil
+}
---
+++ gobdb_test.go
@@ -0,0 +1,117 @@
+package gobdb_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/waltervargas/gobdb"
+)
+
+func TestList(t *testing.T){
+	t.Parallel()
+	db, err := gobdb.Open("tests/list.gobdb")
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+	want := gobdb.Data{
+		1: "walter",
+		2: "barbara",
+		3: "victor",
+	}
+	got := db.List()
+	if !cmp.Equal(want, got){
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestAdd(t *testing.T) {
+	t.Parallel()
+	tmp := t.TempDir() + "/add.gobdb"
+	db, err := gobdb.Open(tmp)
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+	want := gobdb.Data{
+		"answer": 42,
+	}
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestAddStruct(t *testing.T) {
+	t.Parallel()
+
+	tmp := t.TempDir() + "/addStruct.gobdb"
+	db, err := gobdb.Open(tmp)
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+
+	type Prio struct {
+		Name string
+	}
+
+	type Todo struct {
+		Name string
+		Prio Prio
+	}
+
+	todo := Todo{Name: "buy milk", Prio: Prio{"High"}}
+	want := gobdb.Data{
+		"todo": todo,
+	}
+
+	//db.AddSchema(todos)
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestAddStructList(t *testing.T) {
+	t.Parallel()
+
+	tmp := t.TempDir() + "/addStruct.gobdb"
+	db, err := gobdb.Open(tmp)
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+
+	type Prio struct {
+		Name string
+	}
+
+	type Todo struct {
+		Name string
+		Prio Prio
+	}
+
+	todos := []Todo{
+		{Name: "buy milk", Prio: Prio{"High"}},
+		{Name: "tax calculation", Prio: Prio{"High"}},
+	}
+
+	want := gobdb.Data{
+		"todos": todos,
+	}
+
+	//db.AddSchema(todos)
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
