16cfd415976a62f1a06b7ee7cf2dc8a4dc9001ce
Walter Vargas <walter.vargas@n26.com>
2023-04-11 09:27:14 +0200

    Add optional types

--- gobdb_test.go
+++ gobdb_test.go
@@ -115,3 +115,118 @@ func TestAddStructList(t *testing.T) {
 		t.Error(cmp.Diff(want, got))
 	}
 }
+
+
+func TestAddNestedStruct(t *testing.T) {
+	t.Parallel()
+
+	tmp := t.TempDir() + "/addNestedStruct.gobdb"
+	db, err := gobdb.Open(tmp)
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+
+
+	type Reminder struct {
+		What string
+	}
+
+	type Reminders []Reminder
+
+	reminders := Reminders{
+		{What: "buy milk"},
+		{What: "tax calculation"},
+	}
+
+	want := gobdb.Data{
+		"reminders": reminders,
+	}
+
+	//db.AddSchema(todos)
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+// HELP wanted here
+// tried with muttex but stil get dupicated panic error
+// panic: gob: registering duplicate types for "[]gobdb_test.Todo": []gobdb_test.Todo != []gobdb_test.Todo [recovered]
+// panic: gob: registering duplicate types for "[]gobdb_test.Todo": []gobdb_test.Todo != []gobdb_test.Todo
+func TestDuplicatedTypes(t *testing.T) {
+	t.Parallel()
+	t.Skip()
+
+	tmp := t.TempDir() + "/addStruct.gobdb"
+	db, err := gobdb.Open(tmp)
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+
+	type Prio struct {
+		Name string
+	}
+
+	type Todo struct {
+		Name string
+		Prio Prio
+	}
+
+	todos := []Todo{
+		{Name: "buy milk", Prio: Prio{"High"}},
+		{Name: "tax calculation", Prio: Prio{"High"}},
+	}
+
+	want := gobdb.Data{
+		"todos": todos,
+	}
+
+	//db.AddSchema(todos)
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestWithType(t *testing.T) {
+	t.Parallel()
+
+	type Reminder struct {
+		What string
+	}
+
+	type Reminders2 []Reminder
+
+	tmp := t.TempDir() + "/addNestedStruct.gobdb"
+	db, err := gobdb.Open(tmp, gobdb.WithType(Reminders2{}))
+	if err != nil {
+		t.Fatalf("unable to open db: %s", err)
+	}
+
+	reminders := Reminders2{
+		{What: "buy milk"},
+		{What: "tax calculation"},
+	}
+
+	want := gobdb.Data{
+		"reminders": reminders,
+	}
+
+	//db.AddSchema(todos)
+	err = db.Add(want)
+	if err != nil {
+		t.Errorf("unable to add data %v: %s", want, err)
+	}
+	got := db.List()
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
