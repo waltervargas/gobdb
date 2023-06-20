619b9a9d8f472c4110dd4abb3d11540dff4f4c69
Walter Vargas <waltervargas@linux.com>
2023-06-20 17:35:33 +0200

    Refactoring `gobdb` to Use Generics (#4)

--- gobdb.go
+++ gobdb.go
@@ -11,82 +11,110 @@ import (
 )
 
 // Gobdb represents a simple key-value database for Go binary objects encoded
-// with encoding/gob.
-type Gobdb struct {
-	Data Data
-	path string
-	customTypes []any
-}
-
-// Data is a type alias for a map with keys and values of any type.
-type Data map[any]any
-
-func WithType(t any) func(*Gobdb) {
-	return func(db *Gobdb) {
-		db.customTypes = append(db.customTypes, t)
-	}
-}
-
-type option func(*Gobdb)
-
-// Open opens a file at the specified path and decodes its contents using the
-// gob decoder.
+// with encoding/gob. It is a generic type that can store any type of data in
+// its Data field. The path field is unexported and contains the path to the
+// file where the database is stored.
 //
-// If the decoding fails, it returns an error.
-// If the decoding is successful, it returns a gobdb object with the decoded
-// data.
-func Open(path string, opts ...option) (Gobdb, error)  {
-	db := &Gobdb{}
-	for _, optf := range opts {
-		optf(db)
-	}
-	for _, t := range db.customTypes {
-		gob.Register(t)
-	}
+// The Gobdb type parameter T is constrained by the 'any' type constraint, meaning
+// it can be any Go type.
+//
+// Fields:
+//
+//	Data: A slice of type T. It contains the values of the database.
+//	path: A string representing the path to the file where the database is stored.
+type Gobdb[T any] struct {
+	Data []T
+	path string
+}
+
+// Open is a generic function that opens a file at the specified path and
+// decodes its contents using the gob decoder. The type of the data in the
+// file should be specified as the type argument T when calling this function.
+//
+// The type parameter T is constrained by the 'any' type constraint, meaning
+// it can be any Go type.
+//
+// If the decoding fails, it returns an error. If the decoding is successful,
+// it returns a Gobdb object with the decoded data.
+//
+// Parameters:
+//
+//	path: A string representing the path to the file to open and decode.
+//
+// Returns:
+//
+//	A Gobdb object containing the decoded data, and a nil error if the
+//	operation was successful. If an error occurred, the Gobdb object will
+//	be empty and the error will contain details about what went wrong.
+func Open[T any](path string) (Gobdb[T], error) {
+	var t T
+	gob.Register(t)
 
 	file, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0644)
 	if err != nil {
-		return Gobdb{}, err
+		return Gobdb[T]{}, err
 	}
 	defer file.Close()
 
 	decoder := gob.NewDecoder(file)
-	data := make(Data)
+	var data []T
 	err = decoder.Decode(&data)
 	if err != nil {
 		if !errors.Is(err, io.EOF) {
-			return Gobdb{}, err
+			return Gobdb[T]{}, err
 		}
 	}
-	return Gobdb{
+	return Gobdb[T]{
 		Data: data,
 		path: path,
 	}, nil
 }
 
-// List returns the entire database as a map.
-func (db Gobdb) List() Data {
+// List is a method on the Gobdb type that returns all the data in the
+// database as a slice. The type of the data returned is the same as the type
+// parameter T of the Gobdb object.
+//
+// The type parameter T is constrained by the 'any' type constraint, meaning
+// it can be any Go type.
+//
+// This method does not take any parameters.
+//
+// Returns:
+//
+//	A slice of type T containing all the data in the database.
+func (db Gobdb[T]) List() []T {
 	return db.Data
 }
 
-// Add append a new key-value pair of data to the db and persist data to disk
-// using the given `path` to Open()
-func (db *Gobdb) Add(d Data) error {
-	for k, v := range d {
-		// if the caller uses a custom type such as struct as value, then we
-		// need to register this value to gob so it an encode it, otherwise:
-		//
-		//    unable to encode collection: gob: type not registered for interface: []gobdb_test.Todo
-		gob.Register(v)
-		db.Data[k] = v
-
-	}
+// Add is a method on the Gobdb type that adds new data to the database and
+// persists the updated database to disk. The data to be added should be of
+// the same type as the type parameter T of the Gobdb object.
+//
+// The type parameter T is constrained by the 'any' type constraint, meaning
+// it can be any Go type.
+//
+// The method uses the path stored in the Gobdb object to open the file and
+// encodes the updated data using the gob encoder. If an error occurs during
+// this process, it returns an error.
+//
+// Parameters:
+//
+//	d: A variadic parameter of type T representing the data to add to the
+//	   database.
+//
+// Returns:
+//
+//	If the operation is successful, the method returns nil. If an error occurs
+//	during the process, the method returns the error with details about what
+//	went wrong.
+func (db *Gobdb[T]) Add(d ...T) error {
 	file, err := os.Create(db.path)
 	if err != nil {
 		return fmt.Errorf("unable to open file: %w", err)
 	}
 	defer file.Close()
 
+	db.Data = append(db.Data, d...)
 	encoder := gob.NewEncoder(file)
 	err = encoder.Encode(&db.Data)
 	if err != nil {
--- gobdb_test.go
+++ gobdb_test.go
@@ -7,221 +7,28 @@ import (
 	"github.com/waltervargas/gobdb"
 )
 
-func TestList(t *testing.T){
+func TestList(t *testing.T) {
 	t.Parallel()
-	db, err := gobdb.Open("tests/list.gobdb")
+	db, err := gobdb.Open[string]("tests/list.gobdb")
 	if err != nil {
 		t.Fatalf("unable to open db: %s", err)
 	}
-	want := gobdb.Data{
-		1: "walter",
-		2: "barbara",
-		3: "victor",
-	}
+	want := []string{"barbara", "victor", "walter"}
 	got := db.List()
-	if !cmp.Equal(want, got){
+	if !cmp.Equal(want, got) {
 		t.Error(cmp.Diff(want, got))
 	}
 }
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	tmp := t.TempDir() + "/add.gobdb"
-	db, err := gobdb.Open(tmp)
+	path := t.TempDir() + "/add.gobdb"
+	db, err := gobdb.Open[string](path)
 	if err != nil {
 		t.Fatalf("unable to open db: %s", err)
 	}
-	want := gobdb.Data{
-		"answer": 42,
-	}
-	err = db.Add(want)
-	if err != nil {
-		t.Errorf("unable to add data %v: %s", want, err)
-	}
-	got := db.List()
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
-	}
-}
-
-func TestAddStruct(t *testing.T) {
-	t.Parallel()
-
-	tmp := t.TempDir() + "/addStruct.gobdb"
-	db, err := gobdb.Open(tmp)
-	if err != nil {
-		t.Fatalf("unable to open db: %s", err)
-	}
-
-	type Prio struct {
-		Name string
-	}
-
-	type Todo struct {
-		Name string
-		Prio Prio
-	}
-
-	todo := Todo{Name: "buy milk", Prio: Prio{"High"}}
-	want := gobdb.Data{
-		"todo": todo,
-	}
-
-	//db.AddSchema(todos)
-	err = db.Add(want)
-	if err != nil {
-		t.Errorf("unable to add data %v: %s", want, err)
-	}
-	got := db.List()
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
-	}
-}
-
-func TestAddStructList(t *testing.T) {
-	t.Parallel()
-
-	tmp := t.TempDir() + "/addStruct.gobdb"
-	db, err := gobdb.Open(tmp)
-	if err != nil {
-		t.Fatalf("unable to open db: %s", err)
-	}
-
-	type Prio struct {
-		Name string
-	}
-
-	type Todo struct {
-		Name string
-		Prio Prio
-	}
-
-	todos := []Todo{
-		{Name: "buy milk", Prio: Prio{"High"}},
-		{Name: "tax calculation", Prio: Prio{"High"}},
-	}
-
-	want := gobdb.Data{
-		"todos": todos,
-	}
-
-	//db.AddSchema(todos)
-	err = db.Add(want)
-	if err != nil {
-		t.Errorf("unable to add data %v: %s", want, err)
-	}
-	got := db.List()
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
-	}
-}
-
-
-func TestAddNestedStruct(t *testing.T) {
-	t.Parallel()
-
-	tmp := t.TempDir() + "/addNestedStruct.gobdb"
-	db, err := gobdb.Open(tmp)
-	if err != nil {
-		t.Fatalf("unable to open db: %s", err)
-	}
-
-
-	type Reminder struct {
-		What string
-	}
-
-	type Reminders []Reminder
-
-	reminders := Reminders{
-		{What: "buy milk"},
-		{What: "tax calculation"},
-	}
-
-	want := gobdb.Data{
-		"reminders": reminders,
-	}
-
-	//db.AddSchema(todos)
-	err = db.Add(want)
-	if err != nil {
-		t.Errorf("unable to add data %v: %s", want, err)
-	}
-	got := db.List()
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
-	}
-}
-
-// HELP wanted here
-// tried with muttex but stil get dupicated panic error
-// panic: gob: registering duplicate types for "[]gobdb_test.Todo": []gobdb_test.Todo != []gobdb_test.Todo [recovered]
-// panic: gob: registering duplicate types for "[]gobdb_test.Todo": []gobdb_test.Todo != []gobdb_test.Todo
-func TestDuplicatedTypes(t *testing.T) {
-	t.Parallel()
-	t.Skip()
-
-	tmp := t.TempDir() + "/addStruct.gobdb"
-	db, err := gobdb.Open(tmp)
-	if err != nil {
-		t.Fatalf("unable to open db: %s", err)
-	}
-
-	type Prio struct {
-		Name string
-	}
-
-	type Todo struct {
-		Name string
-		Prio Prio
-	}
-
-	todos := []Todo{
-		{Name: "buy milk", Prio: Prio{"High"}},
-		{Name: "tax calculation", Prio: Prio{"High"}},
-	}
-
-	want := gobdb.Data{
-		"todos": todos,
-	}
-
-	//db.AddSchema(todos)
-	err = db.Add(want)
-	if err != nil {
-		t.Errorf("unable to add data %v: %s", want, err)
-	}
-	got := db.List()
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
-	}
-}
-
-func TestWithType(t *testing.T) {
-	t.Parallel()
-
-	type Reminder struct {
-		What string
-	}
-
-	type Reminders2 []Reminder
-
-	tmp := t.TempDir() + "/addNestedStruct.gobdb"
-	db, err := gobdb.Open(tmp, gobdb.WithType(Reminders2{}))
-	if err != nil {
-		t.Fatalf("unable to open db: %s", err)
-	}
-
-	reminders := Reminders2{
-		{What: "buy milk"},
-		{What: "tax calculation"},
-	}
-
-	want := gobdb.Data{
-		"reminders": reminders,
-	}
-
-	//db.AddSchema(todos)
-	err = db.Add(want)
+	want := []string{"barbara", "victor", "walter"}
+	err = db.Add(want...)
 	if err != nil {
 		t.Errorf("unable to add data %v: %s", want, err)
 	}
